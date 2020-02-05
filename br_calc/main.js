2f825ae801fe2c259d0d0d2f8a9d2d65c7162093
ziozyun <ziozyun.coder@gmail.com>
2020-02-05 14:12:46 +0200

    add filter

--- br_calc/main.js
+++ br_calc/main.js
@@ -5,7 +5,13 @@ const elements = {
   tableBody: document.getElementById('table-body'),
   sumBank: document.getElementById('sum-bank'),
   sumRegister: document.getElementById('sum-register'),
-  sumDifference: document.getElementById('sum-difference')
+  sumDifference: document.getElementById('sum-difference'),
+  filterDate: document.getElementById('filter-date'),
+  filterCode: document.getElementById('filter-code'),
+  filterTitle: document.getElementById('filter-title'),
+  filterBank: document.getElementById('filter-bank'),
+  filterRegister: document.getElementById('filter-register'),
+  filterDifference: document.getElementById('filter-difference'),
 };
 
 function parseExcelString(text, columns) {
@@ -38,9 +44,10 @@ function getCode(string) {
   return string;
 }
 
-function getTD(value) {
+function getTD(value, name) {
   const td = document.createElement('td');
   td.innerText = value;
+  td.dataset.name = name;
   return td;
 }
 
@@ -104,13 +111,13 @@ elements.calculate.addEventListener('click', function () {
         if (drawRows[code].dateList.hasOwnProperty(date)) {
           const tr = document.createElement('tr');
           elements.tableBody.appendChild(tr);
-          tr.appendChild(getTD(date));
-          tr.appendChild(getTD(code));
-          tr.appendChild(getTD(drawRows[code].title));
-          tr.appendChild(getTD(drawRows[code].dateList[date].bankData));
-          tr.appendChild(getTD(drawRows[code].dateList[date].registerData));
+          tr.appendChild(getTD(date, 'date'));
+          tr.appendChild(getTD(code, 'code'));
+          tr.appendChild(getTD(drawRows[code].title, 'title'));
+          tr.appendChild(getTD(drawRows[code].dateList[date].bankData, 'bank'));
+          tr.appendChild(getTD(drawRows[code].dateList[date].registerData, 'register'));
           difference = drawRows[code].dateList[date].bankData - drawRows[code].dateList[date].registerData;
-          tr.appendChild(getTD(difference));
+          tr.appendChild(getTD(difference, 'difference'));
           bankSum += drawRows[code].dateList[date].bankData;
           registerSum += drawRows[code].dateList[date].registerData;
           differenceSum += difference;
@@ -122,3 +129,36 @@ elements.calculate.addEventListener('click', function () {
   elements.sumRegister.innerText = registerSum.toString();
   elements.sumDifference.innerHTML = differenceSum.toString();
 });
+
+function filterHandler() {
+  let bankSum = 0, registerSum = 0, differenceSum = 0, display, bank, register, difference;
+  for (const td of elements.tableBody.childNodes) {
+    bank = td.querySelector('[data-name="bank"]').innerText;
+    register = td.querySelector('[data-name="register"]').innerText;
+    difference = td.querySelector('[data-name="difference"]').innerText;
+    if (td.querySelector('[data-name="date"]').innerText.indexOf(elements.filterDate.value) + 1 &&
+      td.querySelector('[data-name="code"]').innerText.indexOf(elements.filterCode.value) + 1 &&
+      td.querySelector('[data-name="title"]').innerText.indexOf(elements.filterTitle.value) + 1 &&
+      bank.indexOf(elements.filterBank.value) + 1 &&
+      register.indexOf(elements.filterRegister.value) + 1 &&
+      difference.indexOf(elements.filterDifference.value) + 1) {
+      display = 'table-row';
+      bankSum += parseFloat(bank);
+      registerSum += parseFloat(register);
+      differenceSum += parseFloat(difference)
+    } else {
+      display = 'none';
+    }
+    td.style.display = display;
+    elements.sumBank.innerText = bankSum.toString();
+    elements.sumRegister.innerText = registerSum.toString();
+    elements.sumDifference.innerHTML = differenceSum.toString();
+  }
+}
+
+elements.filterDate.addEventListener('input', filterHandler);
+elements.filterCode.addEventListener('input', filterHandler);
+elements.filterTitle.addEventListener('input', filterHandler);
+elements.filterBank.addEventListener('input', filterHandler);
+elements.filterRegister.addEventListener('input', filterHandler);
+elements.filterDifference.addEventListener('input', filterHandler);
